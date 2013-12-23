c8e8137f68d00fbd8a22da6b80192d29f0c1a8db
Markus Törnqvist <mjt@fadconsulting.com>
2013-12-23 22:03:59 +0200

    Set a *.csv filter

--- src/csvhandler.cpp
+++ src/csvhandler.cpp
@@ -9,7 +9,11 @@ CSVHandler::CSVHandler(QObject *parent) :
 }
 
 QStringList CSVHandler::getCSVFiles() {
+    QStringList filter;
+    filter << "*.csv";
+
     QDir qdir = QDir(QString("/etc/mersdk/share/"));
+    qdir.setNameFilters(filter);
 
     QStringList list = qdir.entryList();
     return list;
