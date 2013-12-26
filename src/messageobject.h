af58b2ac8a8831a2479e9175396c8c5cdf801388
Markus Törnqvist <mjt@fadconsulting.com>
2013-12-26 22:51:53 +0200

    Steps towards having real properties on the object

--- src/messageobject.h
+++ src/messageobject.h
@@ -8,6 +8,24 @@ class MessageObject : public QObject
     Q_OBJECT
 public:
     explicit MessageObject(QObject *parent = 0);
+    quint32 id;
+    QString eventTypeName;
+    bool isOutgoing;
+    quint64 storageTime;
+    quint64 startTime;
+    quint64 endTime;
+    bool isRead;
+    // XXX: Guesswork
+    quint8 flags;
+    quint32 bytesSent;
+    quint32 bytesReceived;
+    QString localUID;
+    QString localName;
+    QString remoteUID;
+    QString remoteName;
+    QString channel;
+    QString freeText;
+    QString groupUID;
 
 signals:
 
