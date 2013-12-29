2175f1acad5eb64eeafe8c807975ed458ae3b988
Markus Törnqvist <mjt@fadconsulting.com>
2013-12-29 14:23:10 +0200

    Masive refactor

--- src/csvhandler.h
+++ src/csvhandler.h
@@ -15,32 +15,6 @@
 
 typedef QList<MessageObject*> MessageObjectList;
 
-class CSVWorker : public QThread
-{
-    Q_OBJECT
-private:
-    int seenEntries;
-    int seenSMS;
-    QByteArray csvData;
-    QString filepath;
-    Q_INVOKABLE void run();
-    Q_INVOKABLE MessageObjectList actualParse();
-public:
-    explicit CSVWorker(QString filepath);
-    Q_INVOKABLE int getCSVBytes();
-    Q_INVOKABLE int getSeenEntries();
-    Q_INVOKABLE int getSeenSMS();
-    Q_PROPERTY(int readBytes READ getCSVBytes NOTIFY readBytesChanged);
-    Q_PROPERTY(int seenEntries READ getSeenEntries NOTIFY seenEntriesChanged);
-    Q_PROPERTY(int seenSMS READ getSeenSMS NOTIFY seenSMSChanged);
-signals:
-    void readBytesChanged(int newValue);
-    void seenEntriesChanged(int seenEntries);
-    void seenSMSChanged(int seenSMS);
-    void parseFileCompleted(MessageObjectList messages);
-};
-
-
 class CSVHandler : public QObject
 {
     Q_OBJECT
@@ -82,8 +56,5 @@ signals:
 public slots:
 
 };
-
-quint32 toInt(QString s);
-quint64 toLL(QString s);
 
 #endif // CSVHANDLER_H
---
+++ src/csvworker.h
@@ -0,0 +1,41 @@
+#ifndef CSVWORKER_H
+#define CSVWORKER_H
+
+#include <QString>
+#include <QThread>
+
+#ifndef MESSAGEOBJECT_H
+#include "messageobject.h"
+#endif
+
+typedef QList<MessageObject*> MessageObjectList;
+
+class CSVWorker : public QThread
+{
+    Q_OBJECT
+private:
+    int seenEntries;
+    int seenSMS;
+    QByteArray csvData;
+    QString filepath;
+    Q_INVOKABLE void run();
+    Q_INVOKABLE MessageObjectList actualParse();
+public:
+    explicit CSVWorker(QString filepath);
+    Q_INVOKABLE int getCSVBytes();
+    Q_INVOKABLE int getSeenEntries();
+    Q_INVOKABLE int getSeenSMS();
+    Q_PROPERTY(int readBytes READ getCSVBytes NOTIFY readBytesChanged);
+    Q_PROPERTY(int seenEntries READ getSeenEntries NOTIFY seenEntriesChanged);
+    Q_PROPERTY(int seenSMS READ getSeenSMS NOTIFY seenSMSChanged);
+signals:
+    void readBytesChanged(int newValue);
+    void seenEntriesChanged(int seenEntries);
+    void seenSMSChanged(int seenSMS);
+    void parseFileCompleted(MessageObjectList messages);
+};
+
+quint32 toInt(QString s);
+quint64 toLL(QString s);
+
+#endif // CSVWORKER_H
