6db04552edadfc2a9d600212191e0b41125a4bec
Markus Törnqvist <mjt@fadconsulting.com>
2014-01-04 00:00:25 +0200

    Notes, probably useless, on what is needed for libcommhistory

--- src/message.h
+++ src/message.h
@@ -10,23 +10,23 @@ class Message : public QObject
 public:
     explicit Message(QObject *parent = 0);
     quint32 id;
-    QString eventTypeName;
+    QString eventTypeName;  // Doesn't look interesting to Jolla
     bool isOutgoing;
-    quint64 storageTime;
+    quint64 storageTime;    // Apparently ignored on Jolla
     quint64 startTime;
     quint64 endTime;
     bool isRead;
     // XXX: Guesswork
     quint32 flags;
-    quint32 bytesSent;
+    quint32 bytesSent;      // Does not exist on Jolla?
     quint32 bytesReceived;
-    QString localUID;
-    QString localName;
+    QString localUID;       // This is the GROUP_LOCAL_UID hardcode
+    QString localName;      // contacts()? investigate later if neeeded
     QString remoteUID;
-    QString remoteName;
+    QString remoteName;     // Appears empty
     QString channel;
     QString freeText;
-    QString groupUID;
+    QString groupUID;       // Not the same on Jolla
 
 signals:
 
