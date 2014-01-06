1d9b841071dd867439463ab38f0a859fdda65820
Markus Törnqvist <mjt@fadconsulting.com>
2014-01-06 15:22:44 +0200

    Worst kind of copypasta snafu

--- src/glogevent.cpp
+++ src/glogevent.cpp
@@ -4,6 +4,6 @@ GlogEvent::GlogEvent(QObject *parent) :
     QObject(parent)
 {
     this->SMS_TYPE = QString("RTCOM_EL_EVENTTYPE_SMS_MESSAGE");
-    this->CALL_TYPE = QString("RTCOM_EL_EVENTTYPE_CALL_MESSAGE");
-    this->CALL_MISSED_TYPE = QString("RTCOM_EL_EVENTTYPE_CALL_MISSED_MESSAGE");
+    this->CALL_TYPE = QString("RTCOM_EL_EVENTTYPE_CALL");
+    this->CALL_MISSED_TYPE = QString("RTCOM_EL_EVENTTYPE_CALL_MISSED");
 }
